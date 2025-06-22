d905aeef9cfc2862488b1b2b7c694e704a86af70
joris_surface <jorisclement2@gmail.com>
2025-06-22 20:56:52 +0200

    first_PoC

--- llm_app_excel/LLM_excel.py
+++ llm_app_excel/LLM_excel.py
@@ -10,9 +10,60 @@ def ask_LLM (question: str):
 
 def Excel_management():
     
-    excel_path = 'output.xlsx'
-    template_dir = './'
-    template_file = 'email_template.j2'
+    excel_path = './llm_app_excel/output.xlsx'
+    template_dir = './llm_app_excel'
+    template_file = './email_template.j2'
+    output_folder = './llm_app_excel/email_to_client'
+
+    env = Environment(loader=FileSystemLoader(template_dir), autoescape=False)
+    template = env.get_template(template_file)
+
+
+    df = pd.read_excel(excel_path)
+    for index, element in enumerate(df["client"]):
+        excel_name = df.loc[index,"Nom"]
+        excel_surname = df.loc[index,"Prénom"]
+        excel_email = df.loc[index,"email"]
+        excel_fisc_num = df.loc[index,"Numéro fiscal"]
+        excel_addr = df.loc[index,"Adresse"]
+
+
+        #if df.loc[index,"Nom"]  == "Durand":
+        context = {
+        'name': excel_name,
+        'surname': excel_surname,
+        'email': excel_email,
+        'fisc_num': excel_fisc_num,
+        'address' : excel_addr
+        }
+        
+        rendered_text = template.render(context)
+
+        # Save the rendered text to a file
+        with open(output_folder+'/'+excel_name+ '_' + excel_surname +'_output.txt', 'w') as f:
+            f.write(rendered_text)
+
+def main():
+    Excel_management()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     # # Create a DataFrame with an empty column named "test"
@@ -22,19 +73,3 @@ def Excel_management():
     # df.to_excel("./output.xlsx", index=False)
     # #df.to_csv("./output.csv", index=False)
     # print("Excel file created with an empty column named 'test'")
-    env = Environment(loader=FileSystemLoader(template_dir), autoescape=False)
-    template = env.get_template(template_file)
-
-
-    df = pd.read_excel(excel_path)
-    for index, element in enumerate(df["client"]):
-        #if df.loc[index,"Nom"]  == "Durand":
-        print(df.loc[index,"email"])
-
-def main():
-    Excel_management()
-
-
-
-if __name__ == "__main__":
-    main()
