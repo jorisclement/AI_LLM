2b0d56c9cfa6eb0773ebb24d45c29d83df410bc0
joris_surface <jorisclement2@gmail.com>
2025-07-13 13:26:58 +0200

    Working_PoC_v3

--- llm_app_excel/LLM_excel.py
+++ llm_app_excel/LLM_excel.py
@@ -18,17 +18,30 @@ def Excel_management(excel_path, output_folder, template_dir, template_file):
     env = Environment(loader=FileSystemLoader(template_dir), autoescape=False)
     template = env.get_template(template_file)
 
+    # Load all sheets
+    # sheets = pd.read_excel(excel_path, sheet_name=None)  # Returns a dict: {sheet_name: DataFrame}
+    # for sheet_name, df1 in sheets.items():
+    #     print(sheet_name,df1)
 
-    df = pd.read_excel(excel_path)
-    for index, element in enumerate(df["client"]):
-        excel_name = df.loc[index,"Nom"]
-        excel_surname = df.loc[index,"Prénom"]
-        excel_email = df.loc[index,"email"]
-        excel_fisc_num = df.loc[index,"Numéro fiscal"]
-        excel_addr = df.loc[index,"Adresse"]
+    with pd.ExcelFile(excel_path) as xls:
+        sheet_names = xls.sheet_names
+    print(sheet_names)
+
+    df1 = pd.read_excel(excel_path, sheet_name=sheet_names[0])
+    for index, element in enumerate(df1["client"]):
+        excel_name = df1.loc[index,"Nom"]
+        excel_surname = df1.loc[index,"Prénom"]
+        excel_email = df1.loc[index,"email"]
+        excel_fisc_num = df1.loc[index,"Numéro fiscal"]
+        excel_addr = df1.loc[index,"Adresse"]
+
+    df2 = pd.read_excel(excel_path, sheet_name=sheet_names[1])
+    for index, element in enumerate(df2["Code"]):
+        excel_code = df2.loc[index,"Code"]
+        excel_libelle = df2.loc[index,"Libelle"]
 
 
-        #if df.loc[index,"Nom"]  == "Durand":
+        #if df1.loc[index,"Nom"]  == "Durand":
         context = {
         'name': excel_name,
         'surname': excel_surname,
@@ -54,30 +67,47 @@ def main():
 
     Excel_management(args.excel, args.outfolder, args.templatedir, args.template)
 
+def test():
 
 
-if __name__ == "__main__":
-    main()
+    # Create a DataFrame to write
+    new_data = pd.DataFrame({
+        'Nom': ['Durand', 'Martin'],
+        'Prénom': ['Jean', 'Claire'],
+        'Prms': ['OP', 'Claire'],
+        'email': ['jean.durand@email.com', 'claire.martin@email.com']
+    })
 
+    # File path to existing Excel
+    excel_path = 'clients.xlsx'
 
-
-
-
-
-
-
-
-
-
-
-
-
+    # Append new sheet
+    with pd.ExcelWriter(excel_path, engine='openpyxl', mode='a') as writer:
+        new_data.to_excel(writer, sheet_name='sheet2', index=False)
 
 
     # # Create a DataFrame with an empty column named "test"
-    # df = pd.DataFrame(columns=["client","Numéro fiscal","Nom","Prénom","Adresse", "email"])
-    # df["client"] = ["a", "b", "c"] 
+    # df1 = pd.DataFrame(columns=["client","Numéro fiscal","Nom","Prénom","Adresse", "email"])
+    # df1["client"] = ["a", "b", "c"] 
     # # Save it to an Excel file
-    # df.to_excel("./output.xlsx", index=False)
-    # #df.to_csv("./output.csv", index=False)
+    # df1.to_excel("./output.xlsx", index=False)
+    # #df1.to_csv("./output.csv", index=False)
     # print("Excel file created with an empty column named 'test'")
+
+if __name__ == "__main__":
+    main()
+    #test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
